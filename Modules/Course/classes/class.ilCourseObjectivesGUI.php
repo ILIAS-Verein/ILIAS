fc69743d0eaa1a0aa48206534a71d41ce0e845fe
Stefan Meyer <smeyer.ilias@gmx.de>
2015-01-07 13:33:36 +0000

    Fixed test assignment for separated tests
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/optes/50plus@56845 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Course/classes/class.ilCourseObjectivesGUI.php
+++ Modules/Course/classes/class.ilCourseObjectivesGUI.php
@@ -1190,6 +1190,31 @@ class ilCourseObjectivesGUI
 	}
 	
 	/**
+	 * Show test assignment form
+	 * @param ilPropertyFormGUI $form
+	 */
+	protected function finalSeparatedTestAssignment(ilPropertyFormGUI $form = null)
+	{
+		global $ilAccess, $ilErr;
+		
+		if(!$ilAccess->checkAccess('write','',$this->course_obj->getRefId()))
+		{
+			$ilErr->raiseError($this->lng->txt('permission_denied'),$ilErr->WARNING);
+		}
+		if(!$_GET['objective_id'])
+		{
+			ilUtil::sendFailure($this->lng->txt('crs_no_objective_selected'),true);
+			$this->ctrl->returnToParent($this);
+		}
+		$this->ctrl->saveParameter($this,'objective_id');
+		$this->objective = new ilCourseObjective($this->course_obj,(int) $_GET['objective_id']);
+		
+		$this->initWizard(6);
+		$form = $this->initFormTestAssignment();
+		$GLOBALS['tpl']->setContent($form->getHtml());
+	}
+	
+	/**
 	 * self assessment limits
 	 *
 	 * @access protected
@@ -1207,7 +1232,7 @@ class ilCourseObjectivesGUI
 		if(!$_GET['objective_id'])
 		{
 			ilUtil::sendFailure($this->lng->txt('crs_no_objective_selected'),true);
-			$this->ctrl->redirect($this,'listObjectives');
+			$this->ctrl->returnToParent($this);
 		}
 
 		$this->ctrl->saveParameter($this,'objective_id');
@@ -1486,11 +1511,19 @@ class ilCourseObjectivesGUI
 			// checklist gui end
 
 			// begin-patch lok
-			if($step == 3 and !$this->getSettings()->worksWithInitialTest())
+			if($step == 3 and (!$this->getSettings()->worksWithInitialTest() or $this->getSettings()->hasSeparateInitialTests()))
 			{
 				continue;
 			}
-			if($step == 4 and !$this->getSettings()->worksWithInitialTest())
+			if($step == 4 and (!$this->getSettings()->worksWithInitialTest() or $this->getSettings()->hasSeparateInitialTests()))
+			{
+				continue;
+			}
+			if($step == 5 and $this->getSettings()->hasSeparateQualifiedTests())
+			{
+				continue;
+			}
+			if($step == 6 and $this->getSettings()->hasSeparateQualifiedTests())
 			{
 				continue;
 			}
