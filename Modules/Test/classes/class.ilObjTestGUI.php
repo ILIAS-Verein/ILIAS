13ec2ae65bcaef14ce184b9b186247fe9a43df21
Björn Heyser <bheyser@databay.de>
2015-06-30 11:05:11 +0000

    qplskl merge 58029:58030
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/optes/50plus@59666 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Modules/Test/classes/class.ilObjTestGUI.php
+++ Modules/Test/classes/class.ilObjTestGUI.php
@@ -3956,9 +3956,12 @@ class ilObjTestGUI extends ilObjectGUI
 				// skill service
 				if($this->object->isSkillServiceEnabled() && ilObjTest::isSkillManagementGloballyActivated())
 				{
-					require_once 'Modules/Test/classes/class.ilTestSkillQuestionAssignmentsGUI.php';
+					require_once 'Modules/Test/classes/class.ilAssQuestionSkillAssignmentsGUI.php';
 
-					$link = $this->ctrl->getLinkTargetByClass(array('ilTestSkillAdministrationGUI', 'ilTestSkillQuestionAssignmentsGUI'), ilTestSkillQuestionAssignmentsGUI::CMD_SHOW_SKILL_QUEST_ASSIGNS);
+					$link = $this->ctrl->getLinkTargetByClass(
+						array('ilTestSkillAdministrationGUI', 'ilAssQuestionSkillAssignmentsGUI'),
+						ilAssQuestionSkillAssignmentsGUI::CMD_SHOW_SKILL_QUEST_ASSIGNS
+					);
 
 					$tabs_gui->addTarget('tst_tab_competences', $link, array(), array());
 				}
