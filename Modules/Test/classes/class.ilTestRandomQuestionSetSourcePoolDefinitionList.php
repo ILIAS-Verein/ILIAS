664fbac47b314d63e00ec2d5c35eb1c440436c11
Bjoern Heyser <bheyser@databay.de>
2016-08-26 21:31:31 +0200

    the different thinked key part itself .. think different .. be once .. look around .. this is your one way

--- Modules/Test/classes/class.ilTestRandomQuestionSetSourcePoolDefinitionList.php
+++ Modules/Test/classes/class.ilTestRandomQuestionSetSourcePoolDefinitionList.php
@@ -356,4 +356,16 @@ class ilTestRandomQuestionSetSourcePoolDefinitionList implements Iterator
 		echo get_class($this->getTrashedPools()[0]);
 		return array_merge($this->getTrashedPools(), $this->getLostPools());
 	}
+	
+	public function updateSourceQuestionPoolId($oldPoolId, $newPoolId)
+	{
+		foreach($this as $definition)
+		{
+			if($definition->getPoolId() == $oldPoolId)
+			{
+				$definition->setPoolId($newPoolId);
+				$definition->saveToDb();
+			}
+		}
+	}
 }
