9713dd36f89cfb65564b9a7c1255e69dcaa691f1
Bjoern Heyser <bheyser@databay.de>
2016-11-08 10:59:16 +0100

    re-resolve the pool importer merge conflict, that leaded to automatically skipping the patch from core, otherwise no explanation

--- Modules/TestQuestionPool/classes/class.ilTestQuestionPoolImporter.php
+++ Modules/TestQuestionPool/classes/class.ilTestQuestionPoolImporter.php
@@ -66,9 +66,6 @@ class ilTestQuestionPoolImporter extends ilXmlImporter
 		
 		$this->poolOBJ = $newObj;
 		
-		include_once "./Modules/TestQuestionPool/classes/class.ilObjQuestionPool.php";
-		ilObjQuestionPool::_setImportDirectory($this->getImportDirectory());
-		
 		$newObj->fromXML($xml_file);
 		$newObj->saveToDb();
 
